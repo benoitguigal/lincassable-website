00a564f299a3101c7205a556e562d282fa9c3c5f
benoitguigal <benoit.guigal@gmail.com>
2021-06-09 21:40:26 +0200

    display different maps on desktop and mobile

--- src/pages/carte.js
+++ src/pages/carte.js
@@ -2,19 +2,32 @@ import React from "react";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 
-const Carte = () => (
-  <Layout>
-    <SEO title="Carte" />
-    <iframe
-      title="Où trouver / rammener mes bouteilles ?"
-      className="m-0 p-0 pt-12 h-screen"
-      width="100%"
-      height="100%"
-      frameborder="0"
-      allowfullscreen
-      src="//umap.openstreetmap.fr/fr/map/lincassable-ou-trouver-rammener-mes-bouteilles_610505?scaleControl=false&miniMap=false&scrollWheelZoom=true&zoomControl=true&allowEdit=false&moreControl=false&searchControl=null&tilelayersControl=null&embedControl=null&datalayersControl=false&onLoadPanel=undefined&captionBar=true"
-    ></iframe>
-  </Layout>
-);
+const Carte = () => {
+  const iframeProps = {
+    width: "100%",
+    height: "100%",
+    frameborder: "0",
+    allowfullscreen: true
+  };
+
+  return (
+    <Layout>
+      <SEO title="Carte" />
+      {/* render different iframe on mobile and desktop*/}
+      <iframe
+        {...iframeProps}
+        title="Où trouver / rammener mes bouteilles ?"
+        className="hidden md:block m-0 p-0 pt-12 h-screen"
+        src="//umap.openstreetmap.fr/fr/map/lincassable-ou-trouver-rammener-mes-bouteilles_610505?scaleControl=false&miniMap=false&scrollWheelZoom=true&zoomControl=true&allowEdit=false&moreControl=false&searchControl=null&tilelayersControl=null&embedControl=null&datalayersControl=false&onLoadPanel=undefined&captionBar=true&fullscreenControl=false"
+      ></iframe>
+      <iframe
+        {...iframeProps}
+        title="Où trouver / rammener mes bouteilles ? (mobile)"
+        className="md:hidden md:block m-0 p-0 pt-12 h-screen"
+        src="//umap.openstreetmap.fr/fr/map/lincassable-ou-trouver-rammener-mes-bouteilles_610505?scaleControl=false&miniMap=false&scrollWheelZoom=true&zoomControl=false&allowEdit=false&moreControl=false&searchControl=null&tilelayersControl=null&embedControl=null&datalayersControl=false&onLoadPanel=undefined&captionBar=false&fullscreenControl=false"
+      ></iframe>
+    </Layout>
+  );
+};
 
 export default Carte;
