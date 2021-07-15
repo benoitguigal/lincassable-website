24b2287c99ece3e559ea0a39909466549fe78e4f
benoitguigal <benoit.guigal@gmail.com>
2021-07-15 14:59:54 +0200

    update vision

--- src/components/vision.js
+++ src/components/vision.js
@@ -9,7 +9,7 @@ const Vision = () => (
     <div class="grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-12 bg-green-bottle">
       <h1 class="header text-left md:text-right">
         L’AMBITION DE L'INCASSABLE : (RE)DÉVELOPPER UNE FILIÈRE DE RÉEMPLOI DE
-        CONTENANTS EN VERRE EN RÉGION MARSEILLAISE
+        CONTENANTS EN VERRE À MARSEILLE ET DANS LES BOUCHES-DU-RHÔNE
       </h1>
       <div class="text-left md:text-left">
         Fournir, distribuer, collecter, trier, laver, stocker, et redistribuer
