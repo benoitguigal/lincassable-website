82bf81c1516fa497c6ed64019254e7cdaceb64ce
benoitguigal <benoit.guigal@gmail.com>
2022-07-23 15:01:24 +0200

    make some adjustment on producteur component

--- src/components/producteur.tsx
+++ src/components/producteur.tsx
@@ -16,11 +16,11 @@ const Producteur = ({ producteur }: ProducteurProps) => {
         src={producteur.image}
         alt=""
       />
-      <div className="justify-self-start w-full">
+      <div className="justify-self-start self-end pb-10 w-full">
         <h3>{producteur.nom}</h3>
         <div>{producteur.localisation}</div>
       </div>
-      <div>
+      <div className="self-start">
         {producteur.categories.map((category) => (
           <div className="text-xs font-semibold inline-block pt-1 pb-2 px-2 rounded bg-green-bottle uppercase last:mr-0 mr-1">
             {category}
--- src/pages/producteurs.tsx
+++ src/pages/producteurs.tsx
@@ -35,31 +35,31 @@ const ProducteursPage = () => {
   return (
     <Layout>
       <SEO title="Producteurs engagés" />
-      <h1 className="pt-28 md:pt-20 px-5 lg:px-60 mb-6">
-        Producteurs régionaux
-      </h1>
-      <div className="px-5 lg:px-60 grid md:grid-cols-3 grid-rows-1 md:grid-cols-3 gap-4">
-        {producteurs
-          .filter((p) => p.type === "Local")
-          .map((producteur: ProducteurData) => {
-            return <Producteur producteur={producteur} />;
-          })}
-      </div>
-      <h1 className="px-5 lg:px-60 my-6">Producteurs ailleurs en France</h1>
-      <div className="px-5 lg:px-60 grid md:grid-cols-3 grid-rows-1 md:grid-cols-3 gap-4">
-        {producteurs
-          .filter((p) => p.type === "National")
-          .map((producteur: ProducteurData) => {
-            return <Producteur producteur={producteur} />;
-          })}
-      </div>
-      <h1 className="px-5 lg:px-60 my-6">Références Biocoop</h1>
-      <div className="px-5 lg:px-60 grid md:grid-cols-3 grid-rows-1 md:grid-cols-3 gap-4 mb-10">
-        {producteurs
-          .filter((p) => p.type === "Biocoop")
-          .map((producteur: ProducteurData) => {
-            return <Producteur producteur={producteur} />;
-          })}
+      <div className="px-5 lg:px-20 2xl:px-60">
+        <h1 className="pt-28 md:pt-20 mb-6">Producteurs régionaux</h1>
+        <div className="grid md:grid-cols-2 xl:grid-cols-3 grid-rows-1 md:grid-cols-3 gap-4">
+          {producteurs
+            .filter((p) => p.type === "Local")
+            .map((producteur: ProducteurData) => {
+              return <Producteur producteur={producteur} />;
+            })}
+        </div>
+        <h1 className="my-6">Producteurs ailleurs en France</h1>
+        <div className="grid md:grid-cols-2 xl:grid-cols-3 grid-rows-1 md:grid-cols-3 gap-4">
+          {producteurs
+            .filter((p) => p.type === "National")
+            .map((producteur: ProducteurData) => {
+              return <Producteur producteur={producteur} />;
+            })}
+        </div>
+        <h1 className="my-6">Références Biocoop</h1>
+        <div className="grid md:grid-cols-2 xl:grid-cols-3 grid-rows-1 md:grid-cols-3 gap-4 mb-10">
+          {producteurs
+            .filter((p) => p.type === "Biocoop")
+            .map((producteur: ProducteurData) => {
+              return <Producteur producteur={producteur} />;
+            })}
+        </div>
       </div>
       <Footer />
     </Layout>
