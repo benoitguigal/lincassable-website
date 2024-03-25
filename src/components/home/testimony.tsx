e5be208b3c2726caab2764dedc6903dca525a2a2
Benoit Guigal <benoit.guigal@protonmail.com>
2024-03-25 21:20:16 +0100

    add videos

--- src/components/home/testimony.tsx
+++ src/components/home/testimony.tsx
@@ -5,10 +5,30 @@ const Testimony: React.FC = () => {
   return (
     <Section>
       <h1>ILS PARLENT DE LEUR ENGAGEMENT</h1>
-      <div className="grid grid-cols-3">
-        {/* <video preload="auto" controls>
-          <source src={Nathalie} type="video/mp4" />
-        </video> */}
+      <div className="grid grid-cols-3 space-x-6 mt-12">
+        <video preload="false" controls>
+          <source
+            src="https://lincassable-website.s3.fr-par.scw.cloud/videos/ludovic_compressed.mp4"
+            type="video/mp4"
+          />
+        </video>
+
+        <div>
+          <video preload="false" controls>
+            <source
+              src="https://lincassable-website.s3.fr-par.scw.cloud/videos/nathalie_compressed.mp4"
+              type="video/mp4"
+            />
+          </video>
+        </div>
+        <div>
+          <video preload="false" controls>
+            <source
+              src="https://lincassable-website.s3.fr-par.scw.cloud/videos/salem_compressed.mp4"
+              type="video/mp4"
+            />
+          </video>
+        </div>
       </div>
     </Section>
   );
